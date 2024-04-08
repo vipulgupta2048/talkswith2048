79876dced05de1dc7695e9a5718a6b5f1330cb29
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2024-04-08 20:00:31 +0530

    Set production url
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,10 +18,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: `https://mixster.dev`,
+  url: `https://docs.mixster.dev`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/docs/`,
+  baseUrl: `/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
