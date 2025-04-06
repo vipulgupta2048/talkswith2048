f484c6941f7c053f58dd6acc610227c9d91f2e9a
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2025-04-07 03:13:25 +0530

    Add more buttons on homepage
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -9,21 +9,23 @@ export const Hero: FunctionComponent = () => {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <p className={clsx("hero__subtitle", styles.subtitle)}>
-          Welcome to my little corner on the internet,
+          Welcome to my personal docs site!
         </p>
         <h1 className={clsx("hero__title", styles.title)}>
           I am <span className={styles.highlighted}>Vipul Gupta</span> 👋
         </h1>
         <p className={clsx("hero__subtitle", styles.subtitle)}>
-          Thoroughly reads, meticulously documents, and continuously automates,
+          Thoroughly reads, meticulously documents, and continuously automates.
           <br />
           The docs site of a traveling software engineer & cheesecake
           enthusiast.
         </p>
         <br />
-        <Link to="/Introduction" className={clsx("button button--outline button--primary", styles.btn)} > About </Link>
-        <Link to="/category/sharing-what-i-know" className={clsx("button button--outline button--primary", styles.btn)} > Talks </Link>
-        <Link to="/category/past-work" className={clsx("button button--outline button--primary", styles.btn)} > Work </Link>
+        <div className={styles.buttonsContainer}>
+          <Link to="/Introduction" className={clsx("button button--outline button--primary", styles.btn)} > About </Link>
+          <Link to="/category/sharing-what-i-know" className={clsx("button button--outline button--primary", styles.btn)} > Talks </Link>
+          <Link to="/category/past-work" className={clsx("button button--outline button--primary", styles.btn)} > Work </Link>
+        </div>
       </div>
     </header>
   );
